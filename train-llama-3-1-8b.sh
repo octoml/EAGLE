c34641fd290c82c52faab0de7cc51fe703584064
adstraw <astraw@octoml.ai>
2024-09-05 16:18:05 +0000

    must use local path, not HF in train script for now

--- train-llama-3-1-8b.sh
+++ train-llama-3-1-8b.sh
@@ -1,13 +1,16 @@
 export EAGLE_HOME=~/octo-EAGLE
 export TRAIN_DATA_PATH=$EAGLE_HOME/train-data-llama-3-1-8b
-export BASE_MODEL_PATH="meta-llama/Meta-Llama-3.1-8B-Instruct"
+export BASE_MODEL_PATH=/opt/models/Meta-Llama-3.1-8B-Instruct/
 export DRAFT_MODEL_PATH=$EAGLE_HOME/models/EAGLE-LLaMA3.1-Instruct-8B/
 export CONFIG_PATH=$EAGLE_HOME/eagle/train/llama-3-1-8b-instruct_config.json
 
 cd $EAGLE_HOME/eagle/ge_data
 #rm ShareGPT_V4.3_unfiltered_cleaned_split.json
 #wget https://huggingface.co/datasets/Aeala/ShareGPT_Vicuna_unfiltered/blob/main/ShareGPT_V4.3_unfiltered_cleaned_split.json
-/opt/bin/cuda-reserve.py --num-gpus 4 python allocation.py --outdir $TRAIN_DATA_PATH
+/opt/bin/cuda-reserve.py --num-gpus 4 \
+  python allocation.py \
+    --base-model-path $BASE_MODEL_PATH \
+    --outdir $TRAIN_DATA_PATH
 
 cd $EAGLE_HOME
 /opt/bin/cuda-reserve.py --num-gpus 2 \
